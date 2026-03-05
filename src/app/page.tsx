c7a6b25cff5b8018679d9f09c66f64cc0345294c
unknown <akhdanrgya>
2026-03-05 22:49:19 +0700

    Update feeds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { MenuSection } from "@/components/MenuSection";
 import { JourneyTimeline } from "@/components/JourneyTimeline";
 import { VisionValues } from "@/components/VisionValues";
 import { ContactPartnership } from "@/components/ContactPartnership";
+import { InstagramFeed } from "@/components/InstagramFeed";
+import { Testimonials } from "@/components/Testimonials";
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
       <MenuSection />
       <JourneyTimeline />
       <VisionValues />
+      <Testimonials />
+      <InstagramFeed />
       <ContactPartnership />
     </>
   );
